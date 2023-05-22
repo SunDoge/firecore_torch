e9d366e674d9197a580a3333c76c838d358980e0
SunDoge <384813529@qq.com>
2023-05-22 22:42:40 +0800

    add precision to batch processor

--- firecore_torch/batch_processor.py
+++ firecore_torch/batch_processor.py
@@ -30,6 +30,7 @@ class BatchProcessor:
     def __init__(
         self,
         device: torch.device,
+        precision: torch.dtype = torch.float32,
         names: Optional[List[str]] = None,
         rules: Optional[List[str]] = None,
         batch_size_key: Optional[str] = None,
@@ -38,10 +39,12 @@ class BatchProcessor:
         """
         Args:
             device,
+            precision: float precision, can be float32(float), float16(half) or bfloat16(bf16)
             names: for Tuple input
             rules: for Dict input
         """
         self._device = device
+        self._precision = precision
         self._names = names
         self._rules = rules
         self._batch_size_extractor = BatchSizeExtractor(
@@ -55,10 +58,13 @@ class BatchProcessor:
 
         if self._rules is not None:
             batch = adapter.extract(batch, self._rules)
-        
+
         batch_size = self._batch_size_extractor(batch)
 
-        batch_on_device = {k:v.to(self._device, non_blocking=True) for k,v in batch.items()}
+        batch_on_device = {
+            k: self._convert(v)
+            for k, v in batch.items()
+        }
 
         return batch_on_device, batch_size
 
@@ -69,3 +75,9 @@ class BatchProcessor:
             tensor = next(iter(batch.values()))
         batch_size = tensor.size(self._batch_size_index)
         return batch_size
+
+    def _convert(self, x: Tensor):
+        if x.dtype.is_floating_point:
+            return x.to(device=self._device, dtype=self._precision, non_blocking=True)
+        else:
+            return x.to(device=self._device, non_blocking=True)
